36ef9a2aa44b7540cf8fbc8a40a72c11c3ac97d5
ChasenYamashita <32147061+ChasenYamashita@users.noreply.github.com>
2020-03-02 16:08:02 -0800

    dbcon login info

--- dbcon.js
+++ dbcon.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_wolfordj',
-  password        : '7235',
-  database        : 'cs340_wolfordj'
+  user            : 'cs340_yamashch',
+  password        : '0703',
+  database        : 'cs340_yamashch'
 });
 module.exports.pool = pool;
